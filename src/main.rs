98740a6c9271bae5a85d7381221d68c16ff52e36
Matthew Potts <dev.matthewpotts@gmail.com>
2020-12-05 08:57:16 -0700

    Renamed variables and changed some error messages.

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,15 @@ use std::io::stdin;
 use std::io::BufReader;
 use std::net::{TcpListener, TcpStream};
 use std::sync::mpsc::channel;
-use std::sync::mpsc::TryRecvError;
-//use std::sync::mpsc::Receiver;
 use std::sync::mpsc::Sender;
+use std::sync::mpsc::TryRecvError;
 use std::{thread, time};
 
-const SLEEP_LENGTH:time::Duration = time::Duration::from_millis(100);
+const SLEEP_LENGTH: time::Duration = time::Duration::from_millis(100);
 
 fn stream_io_thread(mut stream: TcpStream) -> Sender<std::string::String> {
-    let mut reader = BufReader::new(stream.try_clone().expect("clone failed..."));
-    let (write_sender, write_receiver) = channel::<String>();
+    let mut reader = BufReader::new(stream.try_clone().expect("failed to clone stream."));
+    let (input_sender, input_receiver) = channel::<String>();
 
     thread::spawn(move || loop {
         let mut line = String::new();
@@ -21,46 +20,45 @@ fn stream_io_thread(mut stream: TcpStream) -> Sender<std::string::String> {
             if line != "" {
                 println!("{}", line.trim());
             }
-        }        thread::sleep(SLEEP_LENGTH);
-        match write_receiver.try_recv() {
-            Ok(key) => {
+        }
+        match input_receiver.try_recv() {
+            Ok(usr_input) => {
                 stream
-                    .write(key.as_bytes())
+                    .write(usr_input.as_bytes())
                     .expect("Failed to write to stream");
             }
             Err(TryRecvError::Empty) => {}
             Err(TryRecvError::Disconnected) => panic!("Channel disconnected"),
         }
+        thread::sleep(SLEEP_LENGTH);
     });
-    return write_sender;
+    return input_sender;
 }
 
-fn chat( stream: TcpStream) {
+fn chat(stream: TcpStream) {
     stream
         .set_nonblocking(true)
         .expect("set_nonblocking call failed");
-   // let (write_sender, write_receiver) = channel::<String>();
-  
-   let write_sender = stream_io_thread(stream);
+    let input_sender = stream_io_thread(stream);
     loop {
         thread::sleep(SLEEP_LENGTH);
         let mut usr_input = String::new();
         stdin()
             .read_line(&mut usr_input)
-            .expect("Failed to read line");
-        write_sender
+            .expect("Failed to read from stdin.");
+        input_sender
             .send(usr_input)
             .expect("Failed to send user input.")
     }
 }
 
 fn connect() {
-    let stream = TcpStream::connect("localhost:8080").expect("Couldn't connect to the server...");
+    let stream = TcpStream::connect("localhost:8080").expect("Couldn't connect to the server.");
     chat(stream);
 }
 
 fn listen() -> std::io::Result<()> {
-    let listener = TcpListener::bind("localhost:8080").expect("Couldn't connect to the server...");
+    let listener = TcpListener::bind("localhost:8080").expect("Couldn't connect to the server.");
 
     // accept connections and process them serially
     for stream in listener.incoming() {
@@ -80,8 +78,6 @@ fn main() {
         } else if args[1] == "client" {
             println!("I'm a client");
             connect();
-        } else {
-            println!("i don't know what i am");
         }
     }
 }
