f930c510fed7d3d6d43450a8c5f577cf046a80aa
Matthew Potts <dev.matthewpotts@gmail.com>
2020-12-06 14:29:34 -0700

    Name is entered by the user on program start and displayed next to their message for the other user.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::env;
 use std::io::prelude::*;
 use std::io::stdin;
+use std::io::stdout;
 use std::io::BufReader;
 use std::net::{TcpListener, TcpStream};
+use std::str;
 use std::sync::mpsc::channel;
 use std::sync::mpsc::Sender;
 use std::sync::mpsc::TryRecvError;
@@ -10,15 +12,14 @@ use std::{thread, time};
 
 const SLEEP_LENGTH: time::Duration = time::Duration::from_millis(100);
 
-fn stream_io_thread(mut stream: TcpStream) -> Sender<std::string::String> {
+fn stream_io_thread(mut stream: TcpStream, other_usr: String) -> Sender<std::string::String> {
     let mut reader = BufReader::new(stream.try_clone().expect("failed to clone stream."));
     let (input_sender, input_receiver) = channel::<String>();
-
     thread::spawn(move || loop {
         let mut line = String::new();
         if let Ok(_err) = reader.read_line(&mut line) {
             if line != "" {
-                println!("{}", line.trim());
+                println!("{} : {}", other_usr, line.trim());
             }
         }
         match input_receiver.try_recv() {
@@ -35,11 +36,11 @@ fn stream_io_thread(mut stream: TcpStream) -> Sender<std::string::String> {
     return input_sender;
 }
 
-fn chat(stream: TcpStream) {
+fn chat(stream: TcpStream, usr: &str) {
     stream
         .set_nonblocking(true)
         .expect("set_nonblocking call failed");
-    let input_sender = stream_io_thread(stream);
+    let input_sender = stream_io_thread(stream, usr.to_string());
     loop {
         thread::sleep(SLEEP_LENGTH);
         let mut usr_input = String::new();
@@ -52,19 +53,38 @@ fn chat(stream: TcpStream) {
     }
 }
 
-fn connect() {
-    let stream = TcpStream::connect("localhost:8080").expect("Couldn't connect to the server.");
-    chat(stream);
+fn connect(usr_name: String) {
+    let mut stream = TcpStream::connect("localhost:8080").expect("Couldn't connect to the server.");
+    stream
+        .write(usr_name.as_bytes())
+        .expect("Unable to write user name to stream.");
+    let mut line = [0; 128];
+    stream.read(&mut line).expect("Unable to read stream.");
+    chat(stream, str::from_utf8(&line).unwrap());
 }
 
-fn listen() -> std::io::Result<()> {
+fn listen(usr_name: String) -> std::io::Result<()> {
     let listener = TcpListener::bind("localhost:8080").expect("Couldn't connect to the server.");
 
     // accept connections and process them serially
-    for stream in listener.incoming() {
-        chat(stream?);
+    for tcp_result in listener.incoming() {
+        let mut stream = tcp_result?;
+        stream
+            .write(usr_name.as_bytes())
+            .expect("Unable to write user name to stream.");
+        let mut line = [0; 128];
+        stream.read(&mut line).expect("Unable to read stream.");
+        chat(stream, str::from_utf8(&line).unwrap());
     }
     Ok(())
+}
+
+fn get_usr_name() -> String {
+    print!("Enter your name: ");
+    stdout().flush().expect("unable to flush stdout");
+    let mut usr_name = String::new();
+    stdin().read_line(&mut usr_name).expect("Unable to read stdin.");
+    return usr_name.trim().to_string();
 }
 
 fn main() {
@@ -72,12 +92,12 @@ fn main() {
     if args.len() > 1 {
         if args[1] == "server" {
             println!("I'm a server");
-            if let Err(_err) = listen() {
+            if let Err(_err) = listen(get_usr_name()) {
                 println!("an error occured in listen()")
             }
         } else if args[1] == "client" {
             println!("I'm a client");
-            connect();
+            connect(get_usr_name());
         }
     }
 }
